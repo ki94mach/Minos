7db9a5e183030fa0ac433f69ce2ae118a14ba8b0
ki94mach <kian.mohajeri@gmail.com>
2025-02-16 17:58:58 +0330

    git ignore update

---
+++ src/web_app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '579e4593ab7d119e814e5b1dcd48d26cfd4341802a89750f2324359a12db8a8e'
+    
+    from .routes import main
+    app.register_blueprint(main)
+
+    from pkg.ZODB_manager import RegistryManager
+
+    return app
+
+
